69f2b9fbb31bfb93fc4c47b613e0a63f973a227e
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-11 02:26:33 -0400

    header section with click

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,8 +1,21 @@
-export function Header() {
+export function Header({
+  teamName1,
+  teamName2,
+  onAddScoreTeam1
+}: {
+  teamName1: string
+  teamName2: string
+  onAddScoreTeam1: (score: number) => void
+}) {
   return (
-    <header className='flex justify-around pt-10 sticky'>
-      <h1 className='text-xl font-bold'>Team 1</h1>
-      <h1 className='text-xl font-bold'>Team 2</h1>
+    <header className='flex justify-around pt-10 sticky top-0 bg-slate-500 '>
+      <h1
+        onClick={() => onAddScoreTeam1(10)}
+        className='text-xl font-bold'
+      >
+        {teamName1}
+      </h1>
+      <h1 className='text-xl font-bold'>{teamName2}</h1>
     </header>
   )
 }
