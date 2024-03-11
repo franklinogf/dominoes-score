8cbaab6bcfb26c5cda5e5dea4ecbdcd297eda625
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-11 02:26:04 -0400

    score list section

--- src/sections/ScoreList.tsx
+++ src/sections/ScoreList.tsx
@@ -1,14 +1,31 @@
-export function ScoreList() {
+export function ScoreList({
+  scoreList1,
+  scoreList2
+}: {
+  scoreList1: number[]
+  scoreList2: number[]
+}) {
   return (
     <section className='grid grid-cols-2 mt-4 divide-x-2'>
-      <article className='grid text-center divide-y-2 divide-slate-400/50 px-2'>
-        <span className='font-semibold text-lg text-white py-0.5'>10</span>
-        <span className='font-semibold text-lg text-white py-0.5'>25</span>
-      </article>
-      <article className='grid text-center divide-y-2 divide-slate-400/50 px-2'>
-        <span className='font-semibold text-lg text-white py-0.5'>10</span>
-        <span className='font-semibold text-lg text-white py-0.5'>25</span>
-      </article>
+      <List scoreList={scoreList1} />
+      <List scoreList={scoreList2} />
     </section>
+  )
+}
+
+function List({ scoreList }: { scoreList: number[] }) {
+  return (
+    <article className='grid text-center divide-y-2 divide-slate-400/50 px-2'>
+      {scoreList.map((score, index) => {
+        return (
+          <span
+            key={index}
+            className='font-semibold text-lg text-white py-0.5'
+          >
+            {score}
+          </span>
+        )
+      })}
+    </article>
   )
 }
