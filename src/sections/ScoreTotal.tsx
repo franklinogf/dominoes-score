b9a92e335771ac6163d099c5e247859a91953a97
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-13 18:29:57 -0400

    used new sumScores function to sum the scores

--- src/sections/ScoreTotal.tsx
+++ src/sections/ScoreTotal.tsx
@@ -1,13 +1,18 @@
-import { useTeams } from '../hooks/useTeams'
+import { useTeams, sumScores } from '../hooks/useTeams'
+import { cn } from '../libs/utils'
 
 export function ScoreTotal() {
-  const { team1, team2 } = useTeams()
-  const totalTeam1 = team1.scores.reduce((a, b) => a + b, 0)
-  const totalTeam2 = team2.scores.reduce((a, b) => a + b, 0)
+  const { team1, team2, gameEnded } = useTeams()
+  const totalTeam1 = sumScores(team1.scores)
+  const totalTeam2 = sumScores(team2.scores)
   return (
     <section className='grid grid-cols-2 justify-center text-center border-t-2 border-t-black py-2 mt-auto'>
-      <span className='font-extrabold text-xl text-white'>{totalTeam1}</span>
-      <span className='font-extrabold text-xl text-white'>{totalTeam2}</span>
+      <span className={cn('font-extrabold text-xl text-white', { 'text-green-500': gameEnded })}>
+        {totalTeam1}
+      </span>
+      <span className={cn('font-extrabold text-xl text-white', { 'text-green-500': gameEnded })}>
+        {totalTeam2}
+      </span>
     </section>
   )
 }
