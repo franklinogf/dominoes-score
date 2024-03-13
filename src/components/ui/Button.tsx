6f66d6f71258099ddfc7f800c1843dde1b613a68
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-13 18:26:56 -0400

    button now has the right types

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -13,8 +13,8 @@ const buttonVariants = cva(
         link: 'text-primary underline-offset-4 hover:underline'
       },
       size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
+        default: 'h-9 px-4 py-2',
+        sm: 'h-7 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
         icon: 'h-10 w-10'
       }
@@ -25,24 +25,14 @@ const buttonVariants = cva(
     }
   }
 )
-export function Button({
-  children,
-  variant = 'default',
-  onClick,
-  type = 'button'
-}: {
-  children: React.ReactNode
-  variant?: VariantProps<typeof buttonVariants>['variant']
-  onClick?: () => void
-  type?: HTMLButtonElement['type']
-}) {
+
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> &
+  VariantProps<typeof buttonVariants>
+export function Button({ className, variant, size, ...props }: ButtonProps) {
   return (
     <button
-      onClick={onClick}
-      type={type}
-      className={cn(buttonVariants({ variant }))}
-    >
-      {children}
-    </button>
+      className={cn(buttonVariants({ variant, size, className }))}
+      {...props}
+    ></button>
   )
 }
