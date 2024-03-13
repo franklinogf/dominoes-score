a8010368d01e6a09c1fdc6e14a95fcc87990150f
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-13 18:26:38 -0400

    added a endedGame state tok know when the game has ended

--- src/hooks/useTeams.ts
+++ src/hooks/useTeams.ts
@@ -2,14 +2,16 @@ import { create } from 'zustand'
 import { Team, Teams, TeamsKeys } from '../types/teams'
 
 interface TeamsStore extends Teams {
-  startGame: (name1: Team['name'], name2: Team['name']) => void
+  startGame: (team1Name: Team['name'], team2Name: Team['name']) => void
   endGame: () => void
   addScore: (score: number) => void
-  teamToAddScore: TeamsKeys
-  setTeamToAddScore: (team: TeamsKeys) => void
+  teamToUpdate: TeamsKeys
+  setTeamToUpdate: (team: TeamsKeys) => void
+  limit: number
+  gameEnded: boolean
 }
 
-export const useTeams = create<TeamsStore>((set) => ({
+const initialValues = {
   team1: {
     name: '',
     scores: []
@@ -18,46 +20,36 @@ export const useTeams = create<TeamsStore>((set) => ({
     name: '',
     scores: []
   },
-  teamToAddScore: 'team1',
-  setTeamToAddScore: (team) =>
+  gameEnded: false
+}
+export function sumScores(scores: number[]) {
+  return scores.reduce((a, b) => a + b, 0)
+}
+export const useTeams = create<TeamsStore>((set) => ({
+  ...initialValues,
+  limit: 200,
+
+  teamToUpdate: 'team1',
+  setTeamToUpdate: (team) =>
     set(() => ({
-      teamToAddScore: team
+      teamToUpdate: team
     })),
   startGame: (team1Name, team2Name) =>
     set(() => {
       return { team1: { name: team1Name, scores: [] }, team2: { name: team2Name, scores: [] } }
     }),
-  endGame: () =>
-    set(() => {
-      return {
-        team1: {
-          name: '',
-          scores: []
-        },
-        team2: {
-          name: '',
-          scores: []
-        }
-      }
-    }),
+  endGame: () => set(() => ({ ...initialValues })),
   addScore: (score) =>
     set((state) => {
+      const team = state.teamToUpdate
+      const scores = [...state[team].scores, score]
+      const sumOfScores = sumScores(scores)
       return {
-        [state.teamToAddScore]: {
-          ...state[state.teamToAddScore],
-          scores: [...state[state.teamToAddScore].scores, score]
-        }
+        [team]: {
+          ...state[team],
+          scores
+        },
+        gameEnded: sumOfScores >= state.limit
       }
     })
 }))
-// setShow(false)
-// setTeams({
-//   team1: {
-//     name: team1Name,
-//     scores: []
-//   },
-//   team2: {
-//     name: team2Name,
-//     scores: []
-//   }
-// })
