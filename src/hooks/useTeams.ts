1777f4b4647f5d5888666cc357f3fdde391a8181
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-13 23:34:16 -0400

    edit and delete functions

--- src/hooks/useTeams.ts
+++ src/hooks/useTeams.ts
@@ -5,10 +5,14 @@ interface TeamsStore extends Teams {
   startGame: (team1Name: Team['name'], team2Name: Team['name']) => void
   endGame: () => void
   addScore: (score: number) => void
-  teamToUpdate: TeamsKeys
   setTeamToUpdate: (team: TeamsKeys) => void
+  setScoreIndexToUpdate: (index: number | undefined) => void
+  editScore: (newScore: number) => void
+  deleteScore: () => void
   limit: number
+  scoreIndexToUpdate: number | undefined
   gameEnded: boolean
+  teamToUpdate: TeamsKeys
 }
 
 const initialValues = {
@@ -28,11 +32,15 @@ export function sumScores(scores: number[]) {
 export const useTeams = create<TeamsStore>((set) => ({
   ...initialValues,
   limit: 200,
-
   teamToUpdate: 'team1',
+  scoreIndexToUpdate: undefined,
   setTeamToUpdate: (team) =>
     set(() => ({
       teamToUpdate: team
+    })),
+  setScoreIndexToUpdate: (index) =>
+    set(() => ({
+      scoreIndexToUpdate: index
     })),
   startGame: (team1Name, team2Name) =>
     set(() => {
@@ -50,6 +58,40 @@ export const useTeams = create<TeamsStore>((set) => ({
           scores
         },
         gameEnded: sumOfScores >= state.limit
+      }
+    }),
+  editScore: (newScore) =>
+    set((state) => {
+      if (state.scoreIndexToUpdate === undefined) return {}
+      const team = state.teamToUpdate
+      const scores = state[team].scores
+      scores[state.scoreIndexToUpdate] = newScore
+      const sumOfScores = sumScores(scores)
+
+      return {
+        [team]: {
+          ...state[team],
+          scores
+        },
+        gameEnded: sumOfScores >= state.limit,
+        scoreIndexToUpdate: undefined
+      }
+    }),
+  deleteScore: () =>
+    set((state) => {
+      if (state.scoreIndexToUpdate === undefined) return {}
+      const team = state.teamToUpdate
+      const scores = state[team].scores
+      scores.splice(state.scoreIndexToUpdate, 1)
+      const sumOfScores = sumScores(scores)
+
+      return {
+        [team]: {
+          ...state[team],
+          scores
+        },
+        gameEnded: sumOfScores >= state.limit,
+        scoreIndexToUpdate: undefined
       }
     })
 }))
