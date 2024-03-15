18af2c79ce9df8e10f75e30c8e4b6695e406c327
DarwinML2 <darwinmass.ml2@gmail.com>
2024-03-15 16:20:16 -0400

    used modal ui chakra

--- src/components/NewGameModal.tsx
+++ src/components/NewGameModal.tsx
@@ -1,8 +1,16 @@
 import { useState } from 'react'
-import { cn } from '../libs/utils'
 import { useTeams } from '../hooks/useTeams'
 import { useModals } from '../hooks/useModals'
-import { Button } from '@chakra-ui/react'
+import {
+  Button,
+  Input,
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay
+} from '@chakra-ui/react'
 
 export function NewGameModal() {
   const { startGame } = useTeams()
@@ -15,62 +23,45 @@ export function NewGameModal() {
     hideNewGameModal()
   }
   return (
-    <div
-      id='popup-modal'
-      tabIndex={-1}
-      className={cn(
-        'overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full',
-        { flex: newGameModal, hidden: !newGameModal }
-      )}
+    <Modal
+      isCentered
+      size='sm'
+      isOpen={newGameModal}
+      onClose={hideNewGameModal}
     >
-      <div className='relative p-4 w-full max-w-md max-h-full'>
-        <div className='relative bg-white rounded-lg shadow '>
-          <div className='px-4 py-8 md:p-5 text-center space-y-8'>
-            <div className='relative z-0'>
-              <input
-                type='text'
-                id='small_standard'
-                className='block w-full px-0 py-2 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-                placeholder=''
-                value={team1Name}
-                onChange={(e) => {
-                  setTeam1Name(e.target.value)
-                }}
-              />
-              <label
-                htmlFor='small_standard'
-                className='start-0 absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 '
-              >
-                Team 1
-              </label>
-            </div>
-            <div className='relative z-0'>
-              <input
-                type='text'
-                id='small_standard'
-                className='block w-full px-0 py-2 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-                placeholder=''
-                value={team2Name}
-                onChange={(e) => {
-                  setTeam2Name(e.target.value)
-                }}
-              />
-              <label
-                htmlFor='small_standard'
-                className='start-0 absolute text-sm text-gray-500  duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 peer-focus:text-blue-600  peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6 '
-              >
-                Team 2
-              </label>
-            </div>
-            <Button
-              colorScheme='blue'
-              onClick={handleButtonClick}
-            >
-              Continuar
-            </Button>
-          </div>
-        </div>
-      </div>
-    </div>
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader className='text-center'>
+          Nombres de los equipos.
+        </ModalHeader>
+        <ModalBody className='my-5'>
+          <Input
+            className='text-center my-2'
+            placeholder='Equipo 1'
+            value={team1Name}
+            onChange={(e) => {
+              setTeam1Name(e.target.value)
+            }}
+          />
+          <Input
+            className='text-center'
+            placeholder='Equipo 2'
+            value={team2Name}
+            onChange={(e) => {
+              setTeam2Name(e.target.value)
+            }}
+          />
+        </ModalBody>
+        <ModalFooter className='!justify-center'>
+          <Button
+            size='sm'
+            colorScheme='blue'
+            onClick={handleButtonClick}
+          >
+            Continuar
+          </Button>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   )
 }
