9adf580d273b24874b64312e5872d1891a323185
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-15 15:54:17 -0400

    using the chakra-ui modal

--- src/components/NewScoreModal.tsx
+++ src/components/NewScoreModal.tsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import { cn } from '../libs/utils'
 import {
   Modal,
   ModalOverlay,
@@ -7,22 +6,19 @@ import {
   ModalHeader,
   ModalFooter,
   ModalBody,
-  ModalCloseButton,
   Button,
   ButtonGroup,
-  Input
+  NumberInput,
+  NumberInputField,
+  NumberIncrementStepper,
+  NumberDecrementStepper
 } from '@chakra-ui/react'
 import { useModals } from '../hooks/useModals'
 import { useTeams } from '../hooks/useTeams'
 
 export function NewScoreModal() {
-  const {
-    addScore,
-    scoreIndexToUpdate,
-    editScore,
-    setScoreIndexToUpdate,
-    deleteScore
-  } = useTeams()
+  const { addScore, scoreIndexToUpdate, editScore, setScoreIndexToUpdate, deleteScore, limit } =
+    useTeams()
   const [score, setScore] = useState('')
   const { newScoreModal, hideNewScoreModal } = useModals()
 
@@ -44,69 +40,76 @@ export function NewScoreModal() {
     handleButtonCancel()
   }
   return (
-    <div
-      id='popup-modal'
-      tabIndex={-1}
-      className={cn(
-        'overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full',
-        { flex: newScoreModal, hidden: !newScoreModal }
-      )}
+    <Modal
+      isCentered
+      size='sm'
+      isOpen={newScoreModal}
+      onClose={handleButtonCancel}
     >
-      <Modal>
-        <ModalOverlay />
-        <ModalContent>
-          <ModalHeader>
-            {scoreIndexToUpdate === undefined
-              ? 'Agregar nueva puntuación'
-              : 'Editar puntuación'}
-          </ModalHeader>
-          <ModalBody>
-            <Input
-              type='number'
-              id='small_standard'
-              className='block w-full px-0 py-2 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-blue-600 peer'
-              placeholder=''
+      <ModalOverlay />
+      <ModalContent>
+        <ModalHeader className='text-center'>
+          {scoreIndexToUpdate === undefined ? 'Agregar nueva puntuación' : 'Editar puntuación'}
+        </ModalHeader>
+        <ModalBody>
+          <NumberInput
+            variant={'filled'}
+            min={0}
+            max={limit}
+            size='sm'
+          >
+            <NumberIncrementStepper
+              fontSize={25}
+              textColor={'green.300'}
+              border={0}
+              children='+'
+            />
+            <NumberInputField
+              className='!text-center'
+              p={0}
+              placeholder='Agregar valor'
               value={score}
               onChange={(e) => {
                 setScore(e.target.value)
               }}
             />
-            <ButtonGroup gap={3}>
+
+            <NumberDecrementStepper
+              fontSize={25}
+              textColor={'pink.300'}
+              border={0}
+              children='-'
+            />
+          </NumberInput>
+        </ModalBody>
+        <ModalFooter className='!justify-center'>
+          <ButtonGroup gap={3}>
+            <Button
+              colorScheme='blue'
+              size='sm'
+              onClick={handleButtonClick}
+            >
+              {scoreIndexToUpdate === undefined ? 'Agregar' : 'Editar'}
+            </Button>
+            {scoreIndexToUpdate !== undefined && (
               <Button
-                colorScheme='blue'
+                colorScheme='red'
                 size='sm'
-                onClick={handleButtonClick}
+                onClick={handleDelete}
               >
-                {scoreIndexToUpdate === undefined ? 'Agregar' : 'Editar'}
+                Borrar
               </Button>
-              {scoreIndexToUpdate !== undefined && (
-                <Button
-                  colorScheme='red'
-                  size='sm'
-                  onClick={handleDelete}
-                >
-                  Borrar
-                </Button>
-              )}
-              <Button
-                colorScheme='gray'
-                size='sm'
-                onClick={handleButtonCancel}
-              >
-                Cancelar
-              </Button>
-            </ButtonGroup>
-          </ModalBody>
-          <Button variant='ghost'>Secondary Action</Button>
-        </ModalContent>
-      </Modal>
-      <div className='relative p-4 w-full max-w-md max-h-full'>
-        <div className='relative bg-white rounded-lg shadow '>
-          <div className='px-4 py-8 md:p-5 text-center space-y-8'>
-            <div className='relative z-0'></div>
-          </div>
-        </div>
-      </div>
-    </div>
+            )}
+            <Button
+              colorScheme='gray'
+              size='sm'
+              onClick={handleButtonCancel}
+            >
+              Cancelar
+            </Button>
+          </ButtonGroup>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
   )
 }
