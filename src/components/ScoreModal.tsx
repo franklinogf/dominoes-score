687945ef9110c9cca67423b10145ae90f08c0e64
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-15 18:47:14 -0400

    Score modal using a form again

--- src/components/ScoreModal.tsx
+++ src/components/ScoreModal.tsx
@@ -1,4 +1,3 @@
-import { FormEvent } from 'react'
 import {
   Modal,
   ModalOverlay,
@@ -11,7 +10,8 @@ import {
   NumberInput,
   NumberInputField,
   NumberIncrementStepper,
-  NumberDecrementStepper
+  NumberDecrementStepper,
+  FormControl
 } from '@chakra-ui/react'
 import { useModals } from '../hooks/useModals'
 import { useTeams } from '../hooks/useTeams'
@@ -21,7 +21,7 @@ export function ScoreModal() {
     useTeams()
   const { newScoreModal, hideScoreModal } = useModals()
 
-  function handleButtonClick(e: FormEvent) {
+  function handleFormSubmit(e: React.FormEvent) {
     e.preventDefault()
     const form = e.target as HTMLFormElement
     const formData = new FormData(form)
@@ -51,64 +51,105 @@ export function ScoreModal() {
     >
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader className='text-center'>
+        <ModalHeader textAlign={'center'}>
           {scoreIndexToUpdate === undefined ? 'Agregar nueva puntuación' : 'Editar puntuación'}
         </ModalHeader>
-        <ModalBody>
-          <NumberInput
-            variant={'filled'}
-            min={0}
-            max={limit}
-            size='sm'
-          >
-            <NumberIncrementStepper
-              fontSize={25}
-              textColor={'green.300'}
-              border={0}
-              children='+'
-            />
-            <NumberInputField
-              className='!text-center'
-              p={0}
-              placeholder='Agregar valor'
-            />
-
-            <NumberDecrementStepper
-              fontSize={25}
-              textColor={'pink.300'}
-              border={0}
-              children='-'
-            />
-          </NumberInput>
-        </ModalBody>
-        <ModalFooter className='!justify-center'>
-          <ButtonGroup gap={3}>
-            <Button
-              colorScheme='blue'
-              size='sm'
-              onClick={handleButtonClick}
-            >
-              {scoreIndexToUpdate === undefined ? 'Agregar' : 'Editar'}
-            </Button>
-            {scoreIndexToUpdate !== undefined && (
-              <Button
-                colorScheme='red'
+        <form onSubmit={handleFormSubmit}>
+          <ModalBody>
+            <FormControl>
+              <NumberInput
+                variant={'filled'}
+                min={0}
+                max={limit}
                 size='sm'
-                onClick={handleDelete}
               >
-                Borrar
+                <NumberIncrementStepper
+                  fontSize={25}
+                  textColor={'green.300'}
+                  border={0}
+                  children={<PlustIcon />}
+                />
+                <NumberInputField
+                  className='!text-center'
+                  p={0}
+                  name='score'
+                  placeholder={scoreIndexToUpdate === undefined ? 'Agregar valor' : 'Editar valor'}
+                />
+
+                <NumberDecrementStepper
+                  fontSize={25}
+                  textColor={'pink.300'}
+                  border={0}
+                  children={<MinusIcon />}
+                />
+              </NumberInput>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter justifyContent={'center'}>
+            <ButtonGroup gap={3}>
+              <Button
+                type='submit'
+                colorScheme='blue'
+                size='sm'
+              >
+                {scoreIndexToUpdate === undefined ? 'Agregar' : 'Editar'}
               </Button>
-            )}
-            <Button
-              colorScheme='gray'
-              size='sm'
-              onClick={handleButtonCancel}
-            >
-              Cancelar
-            </Button>
-          </ButtonGroup>
-        </ModalFooter>
+              {scoreIndexToUpdate !== undefined && (
+                <Button
+                  colorScheme='red'
+                  size='sm'
+                  onClick={handleDelete}
+                >
+                  Borrar
+                </Button>
+              )}
+              <Button
+                colorScheme='gray'
+                size='sm'
+                onClick={handleButtonCancel}
+              >
+                Cancelar
+              </Button>
+            </ButtonGroup>
+          </ModalFooter>
+        </form>
       </ModalContent>
     </Modal>
+  )
+}
+function MinusIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className='w-6 h-6'
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M5 12h14'
+      />
+    </svg>
+  )
+}
+function PlustIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth={1.5}
+      stroke='currentColor'
+      className='w-6 h-6'
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M12 4.5v15m7.5-7.5h-15'
+      />
+    </svg>
   )
 }
