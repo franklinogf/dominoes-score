52941dd6c47d982ac02d437aa98688f020daa365
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-15 18:23:28 -0400

    added a button to reset the input

--- src/components/NewGameModal.tsx
+++ src/components/NewGameModal.tsx
@@ -3,13 +3,17 @@ import { useTeams } from '../hooks/useTeams'
 import { useModals } from '../hooks/useModals'
 import {
   Button,
+  IconButton,
   Input,
+  InputGroup,
+  InputRightElement,
   Modal,
   ModalBody,
   ModalContent,
   ModalFooter,
   ModalHeader,
-  ModalOverlay
+  ModalOverlay,
+  Stack
 } from '@chakra-ui/react'
 
 export function NewGameModal() {
@@ -19,6 +23,7 @@ export function NewGameModal() {
   const [team2Name, setTeam2Name] = useState('')
 
   function handleButtonClick() {
+    if (team1Name === '' || team2Name === '') return
     startGame(team1Name, team2Name)
     hideNewGameModal()
   }
@@ -31,26 +36,48 @@ export function NewGameModal() {
     >
       <ModalOverlay />
       <ModalContent>
-        <ModalHeader className='text-center'>
-          Nombres de los equipos.
-        </ModalHeader>
+        <ModalHeader textAlign='center'>Nombres de los equipos.</ModalHeader>
         <ModalBody>
-          <Input
-            className='text-center my-2'
-            placeholder='Equipo 1'
-            value={team1Name}
-            onChange={(e) => {
-              setTeam1Name(e.target.value)
-            }}
-          />
-          <Input
-            className='text-center'
-            placeholder='Equipo 2'
-            value={team2Name}
-            onChange={(e) => {
-              setTeam2Name(e.target.value)
-            }}
-          />
+          <Stack>
+            <InputGroup>
+              <Input
+                textAlign='center'
+                placeholder='Equipo 1'
+                value={team1Name}
+                onChange={(e) => {
+                  setTeam1Name(e.target.value)
+                }}
+              />
+              <InputRightElement>
+                <IconButton
+                  variant='ghost'
+                  fontSize='20px'
+                  aria-label='Limpiar el nombre del equipo 1'
+                  icon={<RestartIcon />}
+                  onClick={() => setTeam1Name('')}
+                />
+              </InputRightElement>
+            </InputGroup>
+            <InputGroup>
+              <Input
+                textAlign='center'
+                placeholder='Equipo 2'
+                value={team2Name}
+                onChange={(e) => {
+                  setTeam2Name(e.target.value)
+                }}
+              />
+              <InputRightElement>
+                <IconButton
+                  variant='ghost'
+                  fontSize='20px'
+                  aria-label='Limpiar el nombre del equipo 2'
+                  icon={<RestartIcon />}
+                  onClick={() => setTeam2Name('')}
+                />
+              </InputRightElement>
+            </InputGroup>
+          </Stack>
         </ModalBody>
         <ModalFooter className='!justify-center'>
           <Button
@@ -63,5 +90,22 @@ export function NewGameModal() {
         </ModalFooter>
       </ModalContent>
     </Modal>
+  )
+}
+
+function RestartIcon() {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      viewBox='0 0 20 20'
+      fill='currentColor'
+      className='w-5 h-5'
+    >
+      <path
+        fillRule='evenodd'
+        d='M7.22 3.22A.75.75 0 0 1 7.75 3h9A2.25 2.25 0 0 1 19 5.25v9.5A2.25 2.25 0 0 1 16.75 17h-9a.75.75 0 0 1-.53-.22L.97 10.53a.75.75 0 0 1 0-1.06l6.25-6.25Zm3.06 4a.75.75 0 1 0-1.06 1.06L10.94 10l-1.72 1.72a.75.75 0 1 0 1.06 1.06L12 11.06l1.72 1.72a.75.75 0 1 0 1.06-1.06L13.06 10l1.72-1.72a.75.75 0 0 0-1.06-1.06L12 8.94l-1.72-1.72Z'
+        clipRule='evenodd'
+      />
+    </svg>
   )
 }
