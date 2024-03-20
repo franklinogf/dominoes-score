86fa844144e584478580acf4436977bde2d53690
Franklin Gonzalez <franklinomarflores@gmail.com>
2024-03-20 00:29:24 -0400

    increase padding to the footer

--- src/sections/ScoreTotal.tsx
+++ src/sections/ScoreTotal.tsx
@@ -6,7 +6,7 @@ export function ScoreTotal() {
   const totalTeam1 = sumScores(teams.team1.scores)
   const totalTeam2 = sumScores(teams.team2.scores)
   return (
-    <section className='fixed left-0 right-0 bottom-0 grid grid-cols-2 justify-center text-center border-t-2 border-t-white py-2 bg-neutral-800/90 z-10'>
+    <section className='fixed left-0 right-0 bottom-0 grid grid-cols-2 justify-center text-center border-t-2 border-t-white py-5 bg-neutral-800/90 z-10'>
       <span className={cn('font-extrabold text-xl text-white', { 'text-green-500': gameEnded })}>
         {totalTeam1}
       </span>
